da34582b91daa8160a6e556d94ce7bedea759c13
Kyle Riedemann <kylealanr@gmail.com>
2015-03-31 11:36:36 -0500

    added GiantbombApiClient to handle retrofit interfaces

---
+++ app/src/main/java/com/kyleriedemann/giantbombvideoplayer/Network/GiantbombApiClient.java
@@ -0,0 +1,27 @@
+package com.kyleriedemann.giantbombvideoplayer.Network;
+
+import java.util.List;
+
+import retrofit.http.GET;
+import retrofit.http.Path;
+
+/**
+ * Created by kyle on 3/31/15.
+ */
+public class GiantbombApiClient {
+    public static final String BASE_URL = "http://www.giantbomb.com/api";
+
+    /*
+    retrofit interface example
+
+    public interface GitHubService {
+        @GET("/users/{user}/repos")
+        List<Repo> listRepos(@Path("user") String user);
+    }
+
+     */
+
+    public interface Videos {
+
+    }
+}
