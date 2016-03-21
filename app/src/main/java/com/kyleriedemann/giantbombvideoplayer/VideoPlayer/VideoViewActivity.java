23b76903bc9357cbf2ee75a42c611a79f9c71a54
Kyle Riedemann <kylealanr@gmail.com>
2016-03-21 18:27:03 -0500

    Added spinner when video is buffering, and save playback position for a given video

--- app/src/main/java/com/kyleriedemann/giantbombvideoplayer/VideoPlayer/VideoViewActivity.java
+++ app/src/main/java/com/kyleriedemann/giantbombvideoplayer/VideoPlayer/VideoViewActivity.java
@@ -1,24 +1,33 @@
 package com.kyleriedemann.giantbombvideoplayer.VideoPlayer;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.VideoView;
 
 import com.kyleriedemann.giantbombvideoplayer.R;
+import com.kyleriedemann.giantbombvideoplayer.Utils.PrefManager;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 
-public class VideoViewActivity extends AppCompatActivity {
+public class VideoViewActivity extends AppCompatActivity implements MediaPlayer.OnCompletionListener, MediaPlayer.OnPreparedListener, MediaPlayer.OnInfoListener {
 
     @Bind(R.id.video_view)
     VideoView videoView;
+
+    @Bind(R.id.video_buffering_spinner)
+    ProgressBar bufferingSpinner;
+
     CustomMediaController mediaController;
+
+    String currentUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +44,16 @@ public class VideoViewActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN );
 
+        videoView.setOnCompletionListener(this);
+        videoView.setOnPreparedListener(this);
+        videoView.setOnInfoListener(this);
+
         String url = getIntent().getExtras().getString("url");
+
+        currentUrl = url;
+
         Uri uri = Uri.parse(url);
         videoView.setVideoURI(uri);
-
-        videoView.start();
     }
 
     @Override
@@ -54,6 +68,31 @@ public class VideoViewActivity extends AppCompatActivity {
                             | View.SYSTEM_UI_FLAG_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        int position = PrefManager.with(this).getInt(currentUrl, 0);
+        if (videoView != null) videoView.seekTo(position);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        PrefManager.with(this).save(currentUrl, videoView.getCurrentPosition());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 
     @Override
@@ -77,5 +116,32 @@ public class VideoViewActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        PrefManager.with(this).remove(currentUrl);
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        int position = PrefManager.with(this).getInt(currentUrl, 0);
+        videoView.seekTo(position);
+
+        videoView.start();
+    }
+
+    @Override
+    public boolean onInfo(MediaPlayer mp, int what, int extra) {
+        if (MediaPlayer.MEDIA_INFO_VIDEO_RENDERING_START == what) {
+            bufferingSpinner.setVisibility(View.GONE);
+        }
+        if (MediaPlayer.MEDIA_INFO_BUFFERING_START == what) {
+            bufferingSpinner.setVisibility(View.VISIBLE);
+        }
+        if (MediaPlayer.MEDIA_INFO_BUFFERING_END == what) {
+            bufferingSpinner.setVisibility(View.VISIBLE);
+        }
+        return false;
     }
 }
